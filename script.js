87dcc35b4096d30bca0460338e47cd0742528c39
Maike Silva <mikemendessilv@gmail.com>
2023-07-27 13:27:45 -0400

    Emulação de input radio
    Botões de alinhamento do texto com funcionamento ok

--- script.js
+++ script.js
@@ -13,6 +13,66 @@ botoes.forEach(botao => {
     })
 })
 
+const botoesRadio = window.document.querySelectorAll('.btn-radio')
+
+//Variável de controle para simular um input radio 
+let dados = {
+	esquerda: true,
+	centro: false,
+	direita: false
+}
+
+function mudarDados (id) {
+	dados[id] = !dados[id]
+	
+	//Não permite tudo falso
+	valoresDados = Object.values(dados)
+	cont = 0
+	for (valor of valoresDados) {
+		if(!valor){
+			cont++
+		}
+	}
+	if(cont == valoresDados.length){
+		dados[id] = !dados[id]
+	}
+	
+	//Não permite mais de um verdadeiro
+	valoresDados = Object.entries(dados)
+	idMudar = 0
+	for (valor of valoresDados){
+		if(valor[0] != id && valor[1]){
+			idMudar = valor[0]
+			break
+		}
+	}
+	if(idMudar != 0){
+		dados[idMudar] = !dados[idMudar]
+	}
+	
+}
+
+function montarAnimacao(dados, botoesRadio){
+	valor = Object.values(dados)
+	for(let i = 0; i < botoesRadio.length; i++){
+		if(valor[i]){
+			botoesRadio[i].setAttribute('style', 'box-shadow: none; background: rgba(2, 111, 133, 0.5);')
+ 		} else{
+			botoesRadio[i].removeAttribute('style')
+		}
+	}
+}
+
+window.onload = montarAnimacao(dados, botoesRadio)
+
+botoesRadio.forEach(botao => {
+    botao.addEventListener('click', () => {
+		id = botao.getAttribute('id')
+		mudarDados(id)
+		montarAnimacao(dados, botoesRadio)
+    })
+})
+
 function negrito(){
     texto.style.fontWeight = 'bold'
 }
